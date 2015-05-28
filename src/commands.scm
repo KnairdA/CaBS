d4d1fd756351606022197d243d03f091227d02b0
Adrian Kummerlaender <knairda@t-online.de>
2015-05-28 22:56:10 +0200

    Moved storage IO into closures
    The goal was to enable storage directory definition in the UI instead of in the storage layer. This required clearer separation of IO-dependent functionality to prevent leaking the storage path all over the place.
    
    As one can see this new solution works as advertised but increases the complexity. i.e. it once again comes down to the central problem of how to best isolate IO from the rest of the program.
    
    Validation is now performed on the whole storage tree at once instead of trough multiple accessor functions.
    `get` was expanded to better cater for multiple blob requests, i.e. the exit status is set correctly.

--- src/commands.scm
+++ src/commands.scm
@@ -12,28 +12,45 @@
                                      (documentation-description documentation))))
                          commands))))
 
-(define (perform-add sources)
-  (if (not (null-list? sources))
-    (for-each (lambda (source) (print (add-blob (read-all source))))
-              sources)
-    (print (add-blob (read-all (current-input-port))))))
+(define (perform-add #!optional sources)
+  (let ((blob-writer (make-blob-writer
+                       (make-file-writer (current-directory))
+                       (make-identifier-reader (make-directory-reader
+                                                 (current-directory))))))
+    (if (not sources)
+      (print (blob-writer (read-all (current-input-port))))
+      (for-each (lambda (source) (print (blob-writer (read-all source))))
+                sources))))
 
 (define (perform-get hashes)
-  (if (not (null-list? hashes))
-    (for-each (lambda (hash) (print (get-blob hash)))
-              hashes)
-    (exit 1)))
+  (let ((blob-reader (make-blob-reader
+                       (make-identifier-reader
+                         (make-directory-reader (current-directory))))))
+    (cond ((null-list? hashes)   (exit 1))
+          ((= 1 (length hashes)) (let ((path (blob-reader (first hashes))))
+                                   (if (not path)
+                                     (exit 1)
+                                     (print path))))
+          (else (let ((paths (delete #f (map blob-reader hashes))))
+                  (for-each print paths)
+                  (exit (boolean->exit (= (length paths) (length hashes)))))))))
 
 (define (perform-check hashes)
-  (if (null-list? hashes)
-    (exit 1)
-    (exit (boolean->exit (every blob-exists? hashes)))))
+  (let ((blob-reader (make-blob-reader
+                       (make-identifier-reader
+                         (make-directory-reader (current-directory))))))
+    (if (null-list? hashes)
+      (exit 1)
+      (exit (boolean->exit (every string? (map blob-reader hashes)))))))
 
 (define (perform-digest)
-  (print (get-storage-digest)))
+  (print (get-storage-digest (make-directory-reader (current-directory)))))
 
 (define (perform-validate)
-  (let ((invalid-nodes (concatenate (map get-invalid-nodes (get-branches)))))
+  (let* ((directory-reader  (make-directory-reader (current-directory)))
+         (identifier-reader (make-identifier-reader directory-reader))
+         (blob-reader       (make-blob-reader       identifier-reader))
+         (invalid-nodes     (get-invalid-nodes directory-reader blob-reader)))
     (if (= 0 (length invalid-nodes))
       (exit 0)
       (begin (for-each print invalid-nodes)
--- src/storage.scm
+++ src/storage.scm
@@ -8,82 +8,92 @@
 (define branch-length 2)
 (define node-length   (- hash-length branch-length))
 
-;; hash io
+;; hash
 
 (define (blob->hash blob)
   (message-digest-object (sha256-primitive) blob))
 
 ;; file io
 
-(define (write-file path content)
-  (let ((fileno (file-open path (+ open/wronly open/creat))))
-    (file-write fileno content)
-    (file-close fileno)))
+(define (make-file-writer context)
+  (lambda (path content)
+    (let ((fileno (file-open (conc context "/" path)
+                             (+ open/wronly open/creat))))
+      (file-write fileno content)
+      (file-close fileno))))
 
-(define (identifier-exists? branch node)
-  (regular-file? (identifier->path branch node)))
+(define (make-directory-reader context)
+  (lambda (path)
+    (let ((full-path (conc context "/" path)))
+      (cond ((regular-file? full-path) full-path)
+            ((directory?    full-path) (directory full-path))
+            (else                      #f)))))
 
-;; blob identifier
+;; identifier
+
+(define-record identifier branch node)
 
 (define (hash->identifier hash)
   (let ((hash (string->list hash)))
-    (values (list->string (take hash branch-length))
-            (list->string (drop hash branch-length)))))
+    (make-identifier (list->string (take hash branch-length))
+                     (list->string (drop hash branch-length)))))
 
-(define (identifier->path branch node)
-  (conc branch "/" node))
+(define (identifier->path identifier)
+  (conc (identifier-branch identifier)
+        "/"
+        (identifier-node identifier)))
+
+(define (make-identifier-reader directory-reader)
+  (lambda (identifier)
+    (let* ((identifier-path (identifier->path identifier))
+           (actual-path     (directory-reader identifier-path)))
+      (if (string? actual-path)
+        identifier-path
+        #f))))
 
 ;; blob
 
-(define (blob-exists? hash)
-  (let-values (((branch node) (hash->identifier hash)))
-    (identifier-exists? branch node)))
+(define (make-blob-reader identifier-reader)
+  (lambda (hash)
+    (identifier-reader (hash->identifier hash))))
 
-(define (get-blob hash)
-  (let-values (((branch node) (hash->identifier hash)))
-    (if (identifier-exists? branch node)
-      (identifier->path branch node)
-      #f)))
-
-(define (add-blob source)
-  (let ((hash (blob->hash source)))
-    (let-values (((branch node) (hash->identifier hash)))
-      (if (not (identifier-exists? branch node))
-        (begin (create-directory branch)
-               (write-file (identifier->path branch node)
-                           source))))
-    hash))
-
-(define (remove-blob hash)
-  (let-values (((branch node) (hash->identifier hash)))
-    (if (identifier-exists? branch node)
-      (delete-file (identifier->path branch node)))
-    (identifier-exists? branch node)))
+(define (make-blob-writer file-writer identifier-reader)
+  (lambda (source)
+    (let* ((hash       (blob->hash source))
+           (identifier (hash->identifier hash)))
+      (if (not (identifier-reader identifier))
+        (begin (create-directory (identifier-branch identifier))
+               (file-writer (identifier->path identifier)
+                            source)))
+      (identifier-reader identifier))))
 
 ;; storage
 
-(define (get-branches)
-  (filter (lambda (candidate) (and (directory? candidate)
-                                   (= branch-length (string-length candidate))))
-          (directory)))
+(define (get-storage-tree directory-reader)
+  (let ((is-branch? (lambda (candidate)
+                      (and (list? (directory-reader candidate))
+                           (= branch-length (string-length candidate)))))
+        (is-node?   (lambda (candidate)
+                      (and (string? candidate)
+                           (= node-length (string-length candidate))))))
+    (map (lambda (branch) (list (car branch)
+                                (filter is-node? (cdr branch))))
+         (map (lambda (branch) (cons branch (directory-reader branch)))
+              (directory-reader ".")))))
 
-(define (get-nodes branch)
-  (filter (lambda (candidate) (and (regular-file? (conc branch "/" candidate))
-                                   (= node-length (string-length candidate))))
-          (directory branch)))
+(define (tree->hashes tree)
+  (concatenate (map (lambda (branch) (map (lambda (node) (conc (car branch) node))
+                                          (second branch)))
+                    tree)))
 
-(define (get-invalid-nodes branch)
-  (remove (lambda (node)
-            (equal? (blob->hash (read-all (conc branch "/" node)))
-                    (conc branch node)))
-          (get-nodes branch)))
+(define (get-invalid-nodes directory-reader blob-reader)
+  (remove (lambda (hash)
+            (equal? (blob->hash (read-all (directory-reader (blob-reader hash))))
+                    hash))
+          (tree->hashes (get-storage-tree directory-reader))))
 
-(define (get-storage)
-  (concatenate (map (lambda (branch) (map (lambda (node) (conc branch node))
-                                          (get-nodes branch)))
-                    (get-branches))))
-
-(define (get-storage-digest)
+(define (get-storage-digest directory-reader)
   (blob->hash (fold conc
                     (string)
-                    (sort (get-storage) string<?))))
+                    (sort (tree->hashes (get-storage-tree directory-reader))
+                          string<?))))
