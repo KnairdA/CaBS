a6bf30059f3724a4ebf666baea653297b3e41d0b
Adrian Kummerlaender <knairda@t-online.de>
2015-05-29 11:48:18 +0200

    Fixed missing argument handling in `get` and `check`
    Also removed unnecessary special case for single element hash lists in `get`.

--- src/commands.scm
+++ src/commands.scm
@@ -22,25 +22,24 @@
       (for-each (lambda (source) (print (blob-writer (read-all source))))
                 sources))))
 
-(define (perform-get hashes)
-  (let ((blob-reader (make-blob-reader
-                       (make-identifier-reader
-                         (make-directory-reader (current-directory))))))
-    (cond ((null-list? hashes)   (exit 1))
-          ((= 1 (length hashes)) (let ((path (blob-reader (first hashes))))
-                                   (if (not path)
-                                     (exit 1)
-                                     (print path))))
-          (else (let ((paths (delete #f (map blob-reader hashes))))
-                  (for-each print paths)
-                  (exit (boolean->exit (= (length paths) (length hashes)))))))))
+(define (perform-get #!optional hashes)
+  (if (not hashes)
+    (exit 1)
+    (let* ((blob-reader (make-blob-reader
+                          (make-identifier-reader
+                            (make-directory-reader (current-directory)))))
+           (paths       (delete #f
+                                (map blob-reader hashes))))
+      (for-each print paths)
+      (exit (boolean->exit (= (length paths)
+                              (length hashes)))))))
 
-(define (perform-check hashes)
-  (let ((blob-reader (make-blob-reader
-                       (make-identifier-reader
-                         (make-directory-reader (current-directory))))))
-    (if (null-list? hashes)
-      (exit 1)
+(define (perform-check #!optional hashes)
+  (if (not hashes)
+    (exit 1)
+    (let ((blob-reader (make-blob-reader
+                         (make-identifier-reader
+                           (make-directory-reader (current-directory))))))
       (exit (boolean->exit (every string? (map blob-reader hashes)))))))
 
 (define (perform-digest)
